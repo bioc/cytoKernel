8eb82fb470390770eb0c6521e40efdaa131737de
Tusharkanti Ghosh <44294394+tghosh30@users.noreply.github.com>
2021-10-01 12:57:37 -0600

    Delete R directory

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-# R CMD check does not like the variable used
-# by foreach::foreach()
-#
-#' @importFrom utils globalVariables
-   if (getRversion() >= "4.1")
-   globalVariables("h")
-   globalVariables("l")
